bc60efb1f461c92e3a6188c7c7edb6ee06b5e746
MyaTaheri <91988696+MyaTaheri@users.noreply.github.com>
2025-02-28 09:35:04 -0500

    2/28/2025

--- TaheriMya/src/Classes/Class.java
+++ TaheriMya/src/Classes/Class.java
@@ -1,36 +1,103 @@
 package Classes;
+
+import java.util.ArrayList;
+
 public class Class {
 
-	private String[][] chart = new String[5][2];
+	int rows = 2;
+	private String[][] chart = new String[rows][2];
+	private String course;
+	private int studentsEnrolled = 0;
+	private ArrayList<String> waitList = new ArrayList<String>();
 
-	public void addStudent()
+	public Class(String course)
 	{
-
+		this.course = course;
 	}
 
-	public void removeStudent()
-	{
+	public void addStudent(String name)
+    {
+        if (studentsEnrolled < (chart.length * chart[0].length))
+        {
+            for (int i = 0; i < chart.length; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (chart[i][j] == null)
+                    {
+                        chart[i][j] = name;
+                        studentsEnrolled++;
+                        return;  // exit once student is added
+                    }
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Class is full. Adding to waitlist...");
+            addToWaitlist(name);
+        }
+    }
 
-	}
+	public void removeStudent(String name)
+    {
+        for (int i = 0; i < chart.length; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                if (chart[i][j] != null && chart[i][j].equals(name))
+                {
+                    chart[i][j] = null;
+                    studentsEnrolled--;
 
-	public void namesOfStudents()
-	{
-
-	}
+                    if (waitList.size() > 0)
+                    {
+                        chart[i][j] = waitList.get(0);
+                        waitList.remove(0);
+                        System.out.println("Enrolled waitlisted student: " + chart[i][j]);
+                    }
+                    return;
+                }
+            }
+        }
+        System.out.println("Student not found in class.");
+    }
 
     public void printNames()
     {
-
+		for (int i = 0; i < chart.length; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				if (chart[i][j] == null)
+					System.out.print ("empty ");
+				else
+					System.out.print(chart[i][j] + " ");
+			}
+		}
+		System.out.println();
     }
 
 	public void printSeatingChart()
 	{
 
+		for (int i = 0; i < chart.length; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				if (chart[i][j] == null) {
+					System.out.print("empty ");
+				}
+				else
+					System.out.print(chart[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
-	public void addToWaitlist()
+	public void addToWaitlist(String name)
 	{
-
+		waitList.add(name);
 	}
 
 }
--- TaheriMya/src/Classes/ClassesDriver.java
+++ TaheriMya/src/Classes/ClassesDriver.java
@@ -6,10 +6,10 @@ public class ClassesDriver
     public static void main(String[] args)
 	{
 		Scanner kb = new Scanner(System.in);
-		int turn = 0;
-		Class calculus = new Class();
-		Class physics = new Class();
-		while(turn != 9)
+		Class calculus = new Class("Calculus");
+		Class physics = new Class("Physics");
+		int x = 0;
+		while(x != 100)
 		{
 			System.out.println("Enter 1 for Calculus Class");
 			System.out.println("Enter 2 for Physics Class");
@@ -18,33 +18,57 @@ public class ClassesDriver
 			{
 				System.out.println("Enter 3 to add student");
 				System.out.println("Enter 4 to remove student");
-				System.out.println("Enter 5 to names of students");
+				System.out.println("Enter 5 to print out names of students");
 				System.out.println("Enter 6 to print out seating chart");
 				System.out.println("Enter 7 to add to wait list");
-				if(kb.nextInt() == 3)
+				System.out.println("Enter 100 to END program");
+				x = kb.nextInt();
+				if(x == 3)
 				{
-					calculus.addStudent();
+					System.out.println("Enter name for student to add: ");
+					calculus.addStudent(kb.next());
 				}
-                else if (kb.nextInt() == 4)
+                else if (x == 4)
                 {
-                    calculus.removeStudent();
+					System.out.println("Enter name for student to remove: ");
+                    calculus.removeStudent(kb.next());
                 }
-                else if (kb.nextInt() == 5)
+                else if (x == 5)
                 {
                     calculus.printNames();
                 }
-                else if (kb.nextInt() == 6)
+                else if (x == 6)
                 {
                     calculus.printSeatingChart();
-                }
-                else if (kb.nextInt() == 7)
-                {
-                    calculus.addToWaitlist();
                 }
 			}
 			else if(pick == 2)
 			{
-
+				System.out.println("Enter 3 to add student");
+				System.out.println("Enter 4 to remove student");
+				System.out.println("Enter 5 to print out names of students");
+				System.out.println("Enter 6 to print out seating chart");
+				System.out.println("Enter 7 to add to wait list");
+				System.out.println("Enter 100 to END program");
+				x = kb.nextInt();
+				if(x == 3)
+				{
+					System.out.println("Enter name for student to add: ");
+					physics.addStudent(kb.next());
+				}
+                else if (x == 4)
+                {
+					System.out.println("Enter name for student to remove: ");
+                    physics.removeStudent(kb.next());
+                }
+                else if (x == 5)
+                {
+                    physics.printNames();
+                }
+                else if (x == 6)
+                {
+                    physics.printSeatingChart();
+                }
 			}
 		}
 	}
